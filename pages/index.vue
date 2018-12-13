4e9466ce85d5639524ea1d57cbbce24fd40ddcbb
Yoshiki <rrrtcdish2@gmail.com>
2018-12-13 23:19:02 +0900

    Transfer Html to Pug(Jade)

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,14 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        myHomeApp
-      </h1>
-      <h2 class="subtitle">
-        My home controll the Features.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+  section.container
+    div
+      logo
+      h1.title myHomeApp
+      h2.subtitle My home control the Features.
+
+      .links
+        a(href="https://nuxtjs.org/" target="_blank").button--green Documentation
+        a(href="https://github.com/nuxt/nuxt.js" target="_blank").button--grey GitHub
+
 </template>
 
 <script>
